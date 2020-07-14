2c0630416f9b35ad40738cbde1e8fc0f7328704b
dude_from_nirvana <b3ntuz1@gmail.com>
2020-07-14 14:56:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
 db_name = 'events.db'
-outer_url = 'http://pogotools.pythonanywhere.com/'
+outer_url = ''
 table_name = 'events'
 database_schema = ('id', 'event_time', 'event_msg', 'repeat', 'event_end')
